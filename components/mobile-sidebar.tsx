7c8659bf32d662fcbeb8161bad437c812aea24db
AmanKmt <perfectcode09@gmail.com>
2023-10-15 16:35:54 +0530

    deploy error fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,7 +3,7 @@ import { Menu } from "lucide-react";
 import { Sidebar } from "./sidebar";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
     return (
         <Sheet>
             <SheetTrigger className="md:hidden pr-4">
@@ -11,7 +11,7 @@ export const MobileSidebar = () => {
             </SheetTrigger>
 
             <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     );
